b608fd11deb0ea1219daad3291c797417f14e092
Leonardo Cintra <leonardo.ncintra@outlook.com>
2024-12-18 18:57:56 -0300

    :pencil: add cria

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -7,17 +7,24 @@ export interface Project {
 
 export const projects: Project[] = [
   {
+    title: "Tchau Audio!",
+    year: 2024,
+    description:
+      "Uma automação que converte texto em audio para usuarios do whastapp (também para outras plataformas caso necessário). O Whatsapp já tem um recurso nativo que transcreve audio para texto, porem é bem limitado. Esse projeto é uma melhoria muito util para que é deficiente auditivo como eu",
+    url: "https://tchauaudio.my.canva.site/",
+  },
+  {
     title: "Rex Helicoides",
-    year: 2023,
+    year: 2022,
     description:
       "Site desenvolvido em Nextjs que é responsavel por divulgar projetos de produçaõ de helicoides para várias industrias e agricultura.",
     url: "https://www.rexhelicoides.com.br",
   },
   {
-    title: "Tchau Audio!",
-    year: 2024,
+    title: "Cria - Implementos Agricolas",
+    year: 2022,
     description:
-      "Uma automação que converte texto em audio para usuarios do whastapp (também para outras plataformas caso necessário). O Whatsapp já tem um recurso nativo que transcreve audio para texto, porem é bem limitado. Esse projeto é uma melhoria muito util para que é deficiente auditivo como eu",
-    url: "https://example.com/",
+      "Site em Nextjs que é para vendas de maquinas agricolas e implementos para agricultura.",
+    url: "https://cria-site.ypg4r9.easypanel.host/",
   },
 ];
